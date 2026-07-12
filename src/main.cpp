863b941a6d12252f3e1a48cf64e3af5dd45c916c
diazsd <diazsd@iu.edu>
2026-07-12 16:08:39 -0400

    made a rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,6 @@
 #include "window.h"
 #include "shaders.h"
 
-using std::cout;
-
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
@@ -42,7 +40,11 @@ GLuint create_VBO(const float vertices[], const int size) {
     return VBO;
 }
 
+void main2();
+
 int main() {
+    main2();
+    return 0;
     GLFWwindow* window = init_glfw();
     init_glad();
     glViewport(0, 0, 800, 600);
@@ -60,7 +62,6 @@ int main() {
 
     const GLuint vertexShader = compile_vertex_shader(vertexShaderSource);
     const GLuint fragmentShader = compile_fragment_shader(fragmentShaderSource);
-
     const GLuint shaderProgram = create_shader_program(vertexShader, fragmentShader);
 
     while (!glfwWindowShouldClose(window)) {
@@ -76,4 +77,50 @@ int main() {
 
     glDeleteProgram(shaderProgram);
     glfwTerminate();
+}
+
+void main2() {
+    GLFWwindow* window = init_glfw();
+    init_glad();
+    glViewport(0, 0, 800, 600);
+
+    float vertices[] = {
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+       -0.5f, -0.5f, 0.0f,  // bottom left
+       -0.5f,  0.5f, 0.0f   // top left
+   };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    };
+
+    GLuint VAO = create_VAO();
+    GLuint VBO = create_VBO(vertices, sizeof(vertices));
+
+    GLuint EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    const GLuint vertexShader = compile_vertex_shader(vertexShaderSource);
+    const GLuint fragmentShader = compile_fragment_shader(fragmentShaderSource);
+    const GLuint shaderProgram = create_shader_program(vertexShader, fragmentShader);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 }
